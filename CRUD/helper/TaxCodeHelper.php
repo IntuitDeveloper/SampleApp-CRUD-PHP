e95868e2c23fe1a4e512b20e52362832c402400b
hlu2 <Hao_Lu@intuit.com>
2018-03-28 13:18:36 -0700

    Remove duplicated directory

--- CRUD/helper/TaxCodeHelper.php
+++
@@ -1,9 +0,0 @@
-<?php
-class TaxCodeHelper {
-    static function getTaxCode(DataService $dataService) {
-        $allTaxCodes = $dataService->FindAll('TaxCode', 0, 500);
-        return $allTaxCodes[0];
-    }
-    
-}
-?>
